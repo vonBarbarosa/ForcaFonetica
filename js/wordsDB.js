4eb76613fdc34ac715dde690f4d1ba68c219d31d
vonBarbarosa <leobarbosareis@gmail.com>
2016-02-22 00:18:56 -0300

    Increase 6 to 33 words

--- js/wordsDB.js
+++ js/wordsDB.js
@@ -49,7 +49,151 @@ var wordsDB = [
 		id: 5,
 		ortography : "deus",
 		phonology : ["ˈdeʊ̯s","ˈdeʊ̯ʃ"]
-	}
+	},
+    {
+		//ainda a.ˈi͂.də
+		id: 6,
+		ortography : "ainda",
+		phonology : ["a.ˈi͂.də"]
+	},
+    {
+		//amar
+		id: 7,
+		ortography : "amar",
+		phonology : ["a.ˈmɑh","a.ˈmɑx", "a.ˈmɑɾ","a.ˈmɑɹ" ]
+	},
+    {
+		id: 8,
+		ortography : "apenas",
+		phonology : ["a.ˈpe.nəs", "a.ˈpe.nəʃ"]
+	},
+    {
+		id: 9,
+		ortography : "aqueles",
+		phonology : ["aˈke.lɪs", "aˈke.lɪʃ"]
+	},
+    {
+		id: 10,
+		ortography : "aqui",
+		phonology : ["a.ˈki"]
+	},
+    {
+		id: 11,
+		ortography : "assim",
+		phonology : ["a.ˈsi͂"]
+	},
+    {
+		id: 12,
+		ortography : "até",
+		phonology : ["a.ˈtɛ"]
+	},
+    {
+		id: 13,
+		ortography : "bem",
+		phonology : ["ˈbe͂ɪ̯"]
+	},
+    {
+		id: 14,
+		ortography : "bom",
+		phonology : ["ˈbõ"]
+	},
+    {
+		id: 15,
+		ortography : "braços",
+		phonology : ["ˈbɾa.sʊs", "ˈbɾa.sʊʃ"]
+	},
+    {
+		id: 16,
+		ortography : "braços",
+		phonology : ["ˈbɾa.sʊs"]
+	},
+    {
+		id: 17,
+		ortography : "cabeça",
+		phonology : ["ka.ˈbe.sə"]
+	},
+    {
+        id: 18,
+        ortography : "cada",
+        phonology : ["ˈka.də"]
+    },
+    {
+        id: 19,
+        ortography : "casa",
+        phonology : ["ˈka.zə"]
+    },
+    {
+        id: 20,
+        ortography : "caso",
+        phonology : ["ˈka.zʊ"]
+    },
+    {
+        id: 21,
+        ortography : "cem, sem",
+        phonology : ["ˈse͂ɪ̯"]
+    },
+    {
+        id: 22,
+        ortography : "cidade",
+        phonology : ["si.ˈda.dʒɪ", "si.ˈda.dɪ"]
+    },
+    {
+        id: 23,
+        ortography : "cinco",
+        phonology : ["ˈsi͂.kʊ"]
+    },
+    {
+        id: 24,
+        ortography : "coisas",
+        phonology : ["ˈkoɪ̯.zəs", "ˈkoɪ̯.zəʃ"]
+    },
+    {
+        id: 25,
+        ortography : "como",
+        phonology : ["ˈkõ.mʊ"]
+    },
+    {
+        id: 26,
+        ortography : "contudo",
+        phonology : ["ˈkõ.tu.dʊ"]
+    },
+    {
+        id: 27,
+        ortography : "coraçao",
+        phonology : ["ko.ɾa.ˈsãʊ̯"]
+    },
+    {
+        id: 28,
+        ortography : "corpo",
+        phonology : ["ˈkoh.pʊ", "ˈkox.pʊ", "ˈkoɾ.pʊ", "ˈkoɹ.pʊ"]
+    },
+    {
+        id: 29,
+        ortography : "cuja",
+        phonology : ["ˈku.ʒə"]
+    },
+    {
+        id: 30,
+        ortography : "dar",
+        phonology : ["ˈdah", "ˈdax", "ˈdaɾ", "ˈdaɹ"]
+    },
+    {
+        id: 31,
+        ortography : "deixar",
+        phonology : ["deɪ̯.ˈʃah", "deɪ̯.ˈʃax", "deɪ̯.ˈʃaɾ", "deɪ̯.ˈʃaɹ"]
+    },
+    {
+        id: 32,
+        ortography : "depois",
+        phonology : ["ˈde.poɪ̯s", "ˈde.poɪ̯ʃ", "ˈdʒi.poɪ̯s", "ˈdʒi.poɪ̯ʃ"]
+    },
+    {
+        id: 33,
+        ortography : "deve",
+        phonology : ["ˈdɛ.vɪ"]
+    }
+
+
 ];
 
 shuffleArray(wordsDB);
